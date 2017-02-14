5c851604835e860a3d73abedc633112851ee7ea9
eirikrok <eirikrok@stud.ntnu.no>
2017-02-14 18:14:56 +0100

    adac.c final fix DAC

--- adac.c
+++ adac.c
@@ -99,16 +99,14 @@ int main(int argc, char **argv)
         bcm2835_spi_chipSelect(BCM2835_SPI_CS0);    // LTC1451. Get ready for setting the DAC
         temp = counter;
         temp2 = counter;
-        test1 = temp & 0x00FF;
+        test2 = temp & 0x00FF;
         temp = counter;
-        test2 = (temp>>8) & 0x00FF;
+        test1 = (temp>>8) & 0x00FF;
         temp = counter;
         buf[(j+1)*3+1+i*(len*adc_chan+2)] = temp & 0x00FF;      // Mask out bits 1-8 first, then
+        temp = counter;
         buf[(j+1)*3+i*(len*adc_chan+2)] = (temp>>8) & 0x00FF;   // Shift right to mask out bits 9-12(9-16, but the 4 MSB should never be ones anyway)
         bcm2835_spi_writenb(&buf[(j+1)*3+i*(len*adc_chan+2)], 2);      //2 final bytes for the DAC
-        //SHit løsning
-        buf[(j+1)*3+1+i*(len*adc_chan+2)] = temp2 & 0x00FF;      // Mask out bits 1-8 first, then
-        buf[(j+1)*3+i*(len*adc_chan+2)] = (temp2>>8) & 0x00FF;   // Shift right to mask out bits 9-12(9-16, but the 4 MSB should never be ones anyway)
         counter = counter + 1023;
         if (counter > 4095){   //Make sure it starts over instead of increasing further
             counter = 0;
